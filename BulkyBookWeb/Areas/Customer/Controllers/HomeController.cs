ba5d6f579bc9814e4a68f3e8b4fdf3814ca00bec
Manar-Antar <manar.antar@eden-tech.io>
2026-09-09 17:15:33 +0300

    Initial Commit

---
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+using BulkyBook.Business.Services.IServices;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+using BulkyBook.Utiltiy;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly IShoppingCartService _shoppingCartService;
+        public HomeController(IProductService productService, IShoppingCartService shoppingCartService)
+        {
+            _productService = productService;
+            _shoppingCartService = shoppingCartService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProductsAsync(includeCategory: true);
+            return View(products);
+        }
+
+        public async Task<IActionResult> Details(int productId)
+        {
+            var product = await _productService.GetProductByIdAsync(productId, includeCategory: true);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ShoppingCart cart = new()
+            {
+                Product = product,
+                Count = 1,
+                ProductId = productId
+            };
+            return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+            }
+
+            shoppingCart.ApplicationUserId = userId;
+            await _shoppingCartService.AddToCartAsync(shoppingCart);
+            var count = await _shoppingCartService.GetCartCountAsync(userId);
+            HttpContext.Session.SetInt32(SD.SessionCart, count);
+            TempData["success"] = "Item added to cart";
+            return RedirectToAction("Index");
+        }
+
+
+      
+    }
+}
---
+++ BulkyBookWeb/Program.cs
@@ -0,0 +1,94 @@
+using BulkyBook.Business.Services;
+using BulkyBook.Business.Services.IServices;
+using BulkyBook.DataAccess.DbInitializer;
+using BulkyBook.Models;
+using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLConnection"));
+});
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;              // Must have at least one number (0-9)
+    options.Password.RequireLowercase = true;          // Must have at least one lowercase letter (a-z)
+    options.Password.RequireUppercase = true;          // Must have at least one uppercase letter (A-Z)
+    options.Password.RequireNonAlphanumeric = true;    // Must have at least one special character (!@#$%^&*)
+    options.Password.RequiredLength = 7;               // Minimum 7 characters
+    options.Password.RequiredUniqueChars = 1;          // Minimum unique characters
+}).AddDefaultTokenProviders()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddScoped<ICategoryService,CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IShoppingCartService,ShoppingCartService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddScoped<IApplicationUserService, ApplicationUserService>();
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/identity/account/login";
+    options.LogoutPath = "/identity/Account/Logout";
+    options.AccessDeniedPath = "/identity/Account/AccessDenied";
+    options.ExpireTimeSpan = TimeSpan.FromDays(30);
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+Stripe.StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseSession();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+await SeedDatabase();
+
+app.MapControllerRoute(
+    name: "MyArea",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}",
+    defaults: new { area = "Customer" })
+    .WithStaticAssets();
+
+
+app.Run();
+
+async Task SeedDatabase()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        await dbInitializer.InitializeAsync();
+    }
+}
